055690ef3ec04a08455ae1f318a4e74e291d0144
Patrick Smith <pgwsmith@gmail.com>
2015-06-30 20:23:29 +1000

    Initial Commit

---
+++ Reprise/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Reprise
+//
+//  Created by Patrick Smith on 30/06/2015.
+//  Copyright © 2015 Burnt Caramel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Reprise/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Reprise
+//
+//  Created by Patrick Smith on 30/06/2015.
+//  Copyright © 2015 Burnt Caramel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
