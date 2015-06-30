8a001383eb50b35b3ef8a02c90f1dc248ec96f65
Patrick Smith <pgwsmith@gmail.com>
2015-06-30 22:50:56 +1000

    Initial commit with Reprise JSON and Safari View loading.

--- Reprise/FirstViewController.swift
+++ Reprise/FirstViewController.swift
@@ -7,19 +7,113 @@
 //
 
 import UIKit
+import SafariServices
 
-class FirstViewController: UIViewController {
+
+enum LinkLoadingError: ErrorType {
+	case ResourceNotFound
+	case InvalidFile
+}
+
+
+struct Link {
+	var title: String
+	var URL: NSURL
+}
+
+
+class ItemTableViewCell: UITableViewCell {
+	@IBOutlet var titleLabel: UILabel!
+}
+
+
+class FirstViewController: UITableViewController {
+	
+	var links = [Link]()
+	var safariViewController: SFSafariViewController!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		do {
+			try loadLinksFromJSON()
+		}
+		catch {
+			
+		}
+	}
+	
+	func loadLinksFromJSON() throws {
+		let bundle = NSBundle.mainBundle()
+		guard let linkJSONURL = bundle.URLForResource("items", withExtension: "json") else {
+			throw LinkLoadingError.ResourceNotFound
+		}
+		
+		guard let data = NSData(contentsOfURL: linkJSONURL) else {
+			throw LinkLoadingError.InvalidFile
+		}
+		
+		guard let JSON = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions()) as? [String: AnyObject] else {
+			throw LinkLoadingError.InvalidFile
+		}
+		
+		guard let linksJSON = JSON["items"] as? [[String: AnyObject]] else {
+			throw LinkLoadingError.InvalidFile
+		}
+		
+		links.removeAll()
+		for linkJSON in linksJSON {
+			guard let
+				title = linkJSON["title"] as? String,
+				URLString = linkJSON["URL"] as? String,
+				URL = NSURL(string: URLString)
+				else
+			{
+				continue
+			}
+			
+			let link = Link(title: title, URL: URL)
+			links.append(
+				link
+			)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	func linkForIndexPath(indexPath: NSIndexPath) -> Link {
+		return links[indexPath.indexAtPosition(1)]
+	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return links.count
+	}
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("link", forIndexPath: indexPath) as! ItemTableViewCell
+		
+		let link = linkForIndexPath(indexPath)
+		cell.titleLabel.text = link.title
+		
+		return cell
+	}
 
-
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		let link = linkForIndexPath(indexPath)
+		
+		print("didSelectRowAtIndexPath \(link)")
+			
+		safariViewController = SFSafariViewController(URL: link.URL, entersReaderIfAvailable: false)
+		safariViewController.delegate = self
+		presentViewController(safariViewController, animated: true, completion: nil)
+	}
 }
 
+extension FirstViewController: SFSafariViewControllerDelegate {
+	func safariViewControllerDidFinish(controller: SFSafariViewController) {
+		controller.dismissViewControllerAnimated(true, completion: nil)
+	}
+}
